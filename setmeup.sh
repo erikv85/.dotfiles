6fe0f63c387a21c2465f81b81cb97f403e83d618
Erik Vesterlund <evesterl@cisco.com>
2019-02-26 16:08:09 +0100

    improved msg

--- setmeup.sh
+++ setmeup.sh
@@ -3,7 +3,8 @@
 pwd=$(dirname $(realpath $0))
 
 if ! grep -q tobashrc $HOME/.bashrc; then
-    echo -e "\nsource $pwd/.tobashrc" | tee -a $HOME/.bashrc
+    echo -e "\nsource $pwd/.tobashrc" >> $HOME/.bashrc
+    echo "$HOME/.bashrc now sources $pwd/.tobashrc"
 fi
 
 ln -sv $pwd/.vimrc $HOME/.vimrc
