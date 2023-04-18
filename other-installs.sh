ee2071915187345ad49979cdd30942ec10bd97fb
Erik Vesterlund <erik.vesterlund@visma.com>
2023-04-18 14:29:24 +0200

    another fix

--- other-installs.sh
+++ other-installs.sh
@@ -1,4 +1,4 @@
 git clone git@github.com:erikv85/scala-spring-archetype.git ~/scala-spring-archetype
 mvn -f ~/scala-spring-archetype install
-echo 'alias=scala-spring-archetype="mvn archetype:generate -DarchetypeGroupId=io.github -DarchetypeArtifactId=scala-spring-archetype -DarchetypeVersion=1.0-SNAPSHOT"' > ~/.dotfiles/.tobashrc
+echo 'alias scala-spring-archetype="mvn archetype:generate -DarchetypeGroupId=io.github -DarchetypeArtifactId=scala-spring-archetype -DarchetypeVersion=1.0-SNAPSHOT"' > ~/.dotfiles/.tobashrc
 echo "scala-spring archetype (alias) installed."
