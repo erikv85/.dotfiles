6a1e157472da921fc3e2abd489599435a3eabbec
Erik Vesterlund <erik.vesterlund@visma.com>
2024-12-24 18:48:59 +0100

    move sourcing tobashrc to ansible

--- setmeup.sh
+++ setmeup.sh
@@ -11,10 +11,6 @@ cwd=$(dirname $(realpath $0))
 sudo apt install -y ansible
 ansible-playbook -vvv ansible/main.yml -K
 
-if ! grep -q tobashrc $HOME/.bashrc; then
-    echo -e "\nsource $cwd/.tobashrc" | tee -a $HOME/.bashrc
-fi
-
 # TODO: this should only be done for work laptop, and in a different way altogether
 if ! grep -q "set background=dark" $HOME/.vimrc; then
     echo -e "\nset background=dark" | tee -a $HOME/.vimrc
